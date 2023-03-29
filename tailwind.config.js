43ba488f54994316b52b1fc5dbc27c7cf33e28a2
vinit-Mittal <vinitmittal0019@gmail.com>
2023-03-29 18:28:10 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepBlue:"#02042a",
+      },
+      fontFamily:{
+        mullish: ["Mulish","sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
